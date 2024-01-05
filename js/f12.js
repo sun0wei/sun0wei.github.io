9e84ec89d36babc806e558d110101df9c8624430
sun0wei <1810915429@qq.com>
2024-01-05 15:36:43 +0800

    Site updated: 2024-01-05 15:36:43

--- js/f12.js
+++ js/f12.js
@@ -1,15 +1,15 @@
-document.onkeydown = function (e) {
-  if (
-    123 == e.keyCode ||
-    (e.ctrlKey &&
-      e.shiftKey &&
-      (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) ||
-    (e.ctrlKey && 85 === e.keyCode)
-  )
-    return (
-      btf.snackbarShow("嘿，小伙子，别看我源码(╬▔皿▔)╯"),
-      (event.keyCode = 0),
-      (event.returnValue = !1),
-      !1
-    );
-};
+// document.onkeydown = function (e) {
+//   if (
+//     123 == e.keyCode ||
+//     (e.ctrlKey &&
+//       e.shiftKey &&
+//       (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) ||
+//     (e.ctrlKey && 85 === e.keyCode)
+//   )
+//     return (
+//       btf.snackbarShow("嘿，小伙子，别看我源码(╬▔皿▔)╯"),
+//       (event.keyCode = 0),
+//       (event.returnValue = !1),
+//       !1
+//     );
+// };
