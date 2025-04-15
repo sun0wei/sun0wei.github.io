c4d7d7d1142b3a70d9eaa9275fbc81379577eb2c
sun0wei <96163303+sun0wei@users.noreply.github.com>
2025-04-15 19:49:33 +0800

    Site updated: 2025-04-15 19:49:32

--- js/fomal.js
+++ js/fomal.js
@@ -3174,10 +3174,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(https://sourcebucket.s3.bitiful.net/img/yuanshen1.webp);
+    --darkmode-bg:url(https://sourcebucket.s3.bitiful.net/img/yuanshen1.webp);
+    --mobileday-bg: url(https://sourcebucket.s3.bitiful.net/img/yuanshen1.webp);
+    --mobilenight-bg: url(https://sourcebucket.s3.bitiful.net/img/yuanshen1.webp);
   }`;
 }
 // 切换背景主函数
