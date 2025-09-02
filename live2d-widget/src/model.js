fa6f9690a77a99f5a03d0b4005593db6db1fc81e
sun0wei <96163303+sun0wei@users.noreply.github.com>
2025-09-03 00:03:24 +0800

    Site updated: 2025-09-03 00:03:23

--- live2d-widget/src/model.js
+++ live2d-widget/src/model.js
@@ -44,14 +44,14 @@ class Model {
             if (!this.modelList) await this.loadModelList();
             const target = randomSelection(this.modelList.models[modelId]);
             loadlive2d("live2d", `${this.cdnPath}model/${target}/index.json`);
-            showMessage("我去换件衣服，别偷看！", 4000, 10);
+            showMessage("我去换件衣服，别偷看！！", 4000, 10);
         } else {
             // 可选 "rand"(随机), "switch"(顺序)
             fetch(`${this.apiPath}rand_textures/?id=${modelId}-${modelTexturesId}`)
                 .then(response => response.json())
                 .then(result => {
                     if (result.textures.id === 1 && (modelTexturesId === 1 || modelTexturesId === 0)) showMessage("我还没有其他衣服呢！", 4000, 10);
-                    else this.loadModel(modelId, result.textures.id, "我去换件衣服，别偷看！");
+                    else this.loadModel(modelId, result.textures.id, "我去换件衣服，别偷看！！");
                 });
         }
     }
