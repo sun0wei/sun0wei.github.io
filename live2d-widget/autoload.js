0c3c66d6c8ee912e2dbf214e1e1a21d873fbf1c7
sun0wei <96163303+sun0wei@users.noreply.github.com>
2025-09-03 13:03:54 +0800

    Site updated: 2025-09-03 13:03:53

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,8 +1,8 @@
 // live2d_path 参数建议使用绝对路径
-// const live2d_path = "https://cdn.jsdelivr.net/gh/sun0wei/live2d-widget/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/sun0wei/live2d-widget/";
 // const live2d_path = "https://blog.sunwei.online/live2d-widget/";
 // 
-const live2d_path = "/live2d-widget/";
+// const live2d_path = "/live2d-widget/";
 
 // /assets/woff111.woff2
 
